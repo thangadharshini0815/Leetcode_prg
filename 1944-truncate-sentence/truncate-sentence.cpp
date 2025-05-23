c724239ee0dd0b1c1c1dbfa0f77fbcce93ca27c8
Thanga Dharshini.M <50thangadharshini10d@gmail.com>
2025-05-23 10:34:35 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (38.01%) - LeetSync

---
+++ 1944-truncate-sentence/truncate-sentence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string truncateSentence(string s, int k) {
+    string result;
+        int count = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s[i] == ' ') {
+                count++;
+                if (count == k)
+                    break;
+            }
+            result += s[i];
+        }
+     return result;
+    }
+ };
