576fee86650de8c4834eb44adf90e6bea86414c9
Thanga Dharshini.M <50thangadharshini10d@gmail.com>
2025-06-10 21:14:51 +0530

    Time: 713 ms (12.80%) | Memory: 199.8 MB (11.47%) - LeetSync

--- 1830-count-good-meals/count-good-meals.cpp
+++ 1830-count-good-meals/count-good-meals.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-    const int MOD = 1e9 + 7;
-
+const int MOD = 1e9 + 7;
 int countPairs(vector<int>& deliciousness) {
     unordered_map<int, int> freq;
     int count = 0;
-
     for (int num : deliciousness) {
-        for (int power = 1; power <= (1 << 21); power *= 2) { 
-            count = (count + freq[power - num]) % MOD;
+        for (int n = 1; n <= (1 << 21); n *= 2) { 
+            count = (count + freq[n - num]) % MOD;
         }
         freq[num]++;
     }
-
-    return count;
+ return count;
 }
 };
