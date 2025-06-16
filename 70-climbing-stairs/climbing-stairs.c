0df97f43731edf8c93c5cb24b9928cca157a747f
Thanga Dharshini.M <50thangadharshini10d@gmail.com>
2025-06-16 18:48:25 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (96.15%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.c
@@ -0,0 +1,11 @@
+int climbStairs(int n) {
+if (n == 1)  
+        return 1;
+    int a = 1, b = 2;
+    for (int i = 3; i <= n; i++) {
+        int temp = a + b;
+        a = b;
+        b = temp;
+    }
+return b;
+}
